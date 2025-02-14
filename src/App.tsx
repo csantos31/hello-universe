4528174b12e831a48b2a32446814eb20c49c8c45
Cyntia S <cyntia.aparecida@hotmail.com>
2025-02-14 18:38:34 -0300

    chore: test change deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Will be deployed in GH Pages
+          I am deployed in GH Pages
         </p>
       </header>
     </div>
